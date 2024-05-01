33e6678fa32024cf64bd67ce25577180f19a7efd
Ashurov Georgy <ashurov1308@mail.ru>
2024-05-01 14:50:51 +0300

    create registration method

--- life_moments/urls.py
+++ life_moments/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 from life_moments_app import views
+
+router = routers.DefaultRouter()
+
+router.register(r'user', views.UserViewSet, basename='user')
 
 urlpatterns = [
     path('books', views.GetBooks, name='getBooks'),
-    path('login',  views.login_view, name='auth'),
-    path('logout', views.logout_view, name='logout'),
-    path('user_info', views.user_info, name='user_info')
+
+    path('user/register', views.UserViewSet.as_view({'post': 'create'}), name='user_register'),
+    # path('login',  views.login_view, name='auth'),
+    # path('logout', views.logout_view, name='logout'),
+    # path('user_info', views.user_info, name='user_info')
 ]
--- life_moments_app/views.py
+++ life_moments_app/views.py
@@ -5,13 +5,60 @@ from life_moments_app.serializers import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
+from rest_framework import status
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+import uuid
+from django.conf import settings
+import redis
+
+session_storage = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 @api_view(['GET'])
 def GetBooks(request):
     books = Books.objects
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    model_class = CustomUser
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    def create(self, request):
+        print('req is', request.data)
+        if self.model_class.objects.filter(email=request.data['email']).exists():
+            return Response({'status': 'Exist'}, status=400)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            currentDate = datetime.now().strftime('%Y-%m-%d')
+            self.model_class.objects.create_user(
+                username=serializer.data['username'],
+                email=serializer.data['email'],
+                password=serializer.data['password'],
+                profile_picture=serializer.data['profile_picture'],
+                rating=0, # TODO: Сделать функцию расчета рейтинга
+                registration_date=currentDate,
+            )
+            random_key = str(uuid.uuid4())
+            session_storage.set(random_key, serializer.data['email'])
+            user_data = {
+                "username": request.data['username'],
+                "email": request.data['email'],
+                "profile_picture": request.data['profile_picture'],
+                "rating": 0, # TODO: Сделать функцию расчета рейтинга
+                "registration_date": currentDate
+            }
+
+            print('user data is ', user_data)
+            response = Response(user_data, status=status.HTTP_201_CREATED)
+            response.set_cookie("session_id", random_key)
+            return response
+        return Response({'status': 'Error', 'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['Post'])
 @permission_classes([AllowAny])
