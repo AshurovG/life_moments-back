17080cf77a67c258fb3d01820c3631a4faa26d6e
Ashurov Georgy <ashurov1308@mail.ru>
2024-05-01 15:07:57 +0300

    add /api route for all requests

--- life_moments/urls.py
+++ life_moments/urls.py
@@ -4,14 +4,14 @@ from rest_framework import routers
 from life_moments_app import views
 
 router = routers.DefaultRouter()
-
 router.register(r'user', views.UserViewSet, basename='user')
 
-urlpatterns = [
+api_urlpatterns = [
     path('books', views.GetBooks, name='getBooks'),
-
     path('user/register', views.UserViewSet.as_view({'post': 'create'}), name='user_register'),
     path('user/login', views.UserViewSet.as_view({'post': 'login'}), name='user_login'),
-    # path('logout', views.logout_view, name='logout'),
-    # path('user_info', views.user_info, name='user_info')
+]
+
+urlpatterns = [
+    path('api/', include(api_urlpatterns)),
 ]
